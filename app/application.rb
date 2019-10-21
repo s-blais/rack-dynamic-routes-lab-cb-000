c5740373b05a295df93a2f9ab85caf3cbb0820b5
Stephen Blais <stephen@s-blais.com>
2019-10-21 15:15:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
       item_name = req.path.split("/items/").last
       binding.pry
       item = Item.all.find{|item| item.name == item_name}
-      puts item.price
+      resp.write item.price
     else
       resp.write "Route not found"
       resp.status = 404
