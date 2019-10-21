cf47fbdb19ed0a442e2c1bb6b9eac914deb31a1d
Stephen Blais <stephen@s-blais.com>
2019-10-21 15:05:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/")
-      item = Item.items.find{|item| item.name == item_name}
+      item = Item.@@items.find{|item| item.name == item_name}
       item.price
     else
       resp.write "Route not found"
